e2dd77fc2783dc97ec924c673e8946c45cad412c
inix84 <inix84@mail.ru>
2025-01-24 00:13:09 +0500

    искл по лайву 5

--- src/com/skypro/JavaProMain.java
+++ src/com/skypro/JavaProMain.java
@@ -1,7 +1,42 @@
 package com.skypro;
 
+import java.util.IllegalFormatConversionException;
+import java.util.IllegalFormatException;
+
 public class JavaProMain {
     public static void main(String[] args) {
-        System.out.println("Skypro Un 1 - Java Professional start!");
+        System.out.println("Skypro Uni - Java Professional start!");
+        multiExceptionCatch();
+    }
+
+    private static void toTrowOrNotToTrow(boolean exception) throws Exception {
+        System.out.println("JavaProMain.toTrowOrNotToTrow");
+        if (exception) {
+            throw new Exception();
+        }
+        System.out.println("toTrowOrNotToTrow end.");
+    }
+
+    private static void multiExceptionCatch() {
+        try {
+            System.out.printf("Hello, %f", 1);
+        } catch (IllegalFormatConversionException e) { // cначала ловяться дети,
+            System.out.println("caught (поймал) IllegalFormatConversionException!");
+        } catch (IllegalFormatException e) { // а потом ловятся родители
+            System.out.println("caught (поймал) IllegalFormatException!");
+        }
+    }
+
+    private static void throwInCatch(){
+        try {
+            System.out.printf("Hello, %f", 1);
+        } catch (IllegalFormatConversionException e) { // cначала ловяться дети,
+            System.out.println("caught (поймал) IllegalFormatConversionException!");
+        } catch (IllegalFormatException e) { // а потом ловятся родители
+            System.out.println("caught (поймал) IllegalFormatException!");
+        }
+
+
     }
 }
+
