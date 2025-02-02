f3b76935420282810f0761fc87dd1247624c4864
inix84 <inix84@mail.ru>
2025-02-03 00:47:43 +0500

    листы по лайву 6

--- src/com/skypro/JavaProMain.java
+++ src/com/skypro/JavaProMain.java
@@ -1,9 +1,11 @@
 package com.skypro;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 
 public class JavaProMain {
     public static void main(String[] args) throws InterruptedException {
@@ -13,25 +15,44 @@ public class JavaProMain {
         objects.add(new Object());
         objects.add("123");
         objects.add(new Cat("orange", "Murzik"));
+        objects.add(null);
 
         System.out.println("objects = " + objects);
         System.out.println("objects [2] = " + objects.get(2));
-        objects.remove(1); // удаление по индексу, это 123
+        objects.remove(1);
         System.out.println("objects = " + objects);
-        System.out.println("objects [1] = " + objects.get(1));// показать элемент № 1
-        System.out.println("objects [2] = " + objects.get(2));
-        int [] ints = {1,2,3}; // массив интов?  а не лист
-        //System.out.println("ints = " + ints); // выведет ints = [I@2d98a335
-        System.out.println("ints = " +Arrays.toString(ints));
-        ints[1]=0; //remove, но массив не уменьшился в размере
-        System.out.println("ints = " +Arrays.toString(ints));
+        System.out.println("objects [1] = " + objects.get(1));
 
-// Не изменяемый лист List.of, можно добавлять по элементно
-        List<String> strings = List.of ("1","2","3");
-        System.out.println("strings = " +strings);
+        // Не МУТАБЕЛЬНЫЙ лист List.of, можно добавлять по элементно
+        List<String> strings = List.of("1", "2", "3"); // запрещено добавлять элемент нулл
+        System.out.println("strings = " + strings);
 
+        for (String s : strings) {
+            System.out.println("s = " + s);
+            System.out.println("s.length = " + s.length());
+        }
+        // в листе могут быть одинаковые элементы
+        System.out.println("есть 1? = " + strings.contains("1")); // проверяет наличие элемента в составе коллекции
+        System.out.println("есть 5? = " + strings.contains("5"));
 
+        List<Cat> cats = new ArrayList<>(List.of(new Cat("white", "Pet"), new Cat("black", "Vas")));
+        Arrays.asList(); // тоже будет давать неизменяемый лист
+        System.out.println("есть Пет белый? = " + cats.contains(new Cat("white", "Pet")));
+
+        cats.sort((o1, o2) -> o1.name.compareTo(o2.name)); // используем лямбды, переопределим методо Comparator. принимает два обьекта, две кошки, мы должны сравнить их между собой
+        // суть метода, если обьект слева больше чем обьект справа, то 1. если наоборот, то минус 1, если равны, то 0.
+        // мы будем сравнивать по первыой букве имени
+        System.out.println("cats = " + cats);
+
+        List<Integer> integers = List.of(10, 2, 3); // так нельзя, так как вью наследуюет свойства всего листа. Если он не изменяемый, то и отрезок будет не изме.
+        System.out.println("integers = " + integers);
+        List<Integer> subList = integers.subList(0, 1); // вьюшка от индекса 0 до индекса 1, отрезок листа, с которым можно производить операции, которые отражаются и во всем листа
+        System.out.println("subList = " + subList);
+        subList.addAll(List.of(4, 5, 6));
+        System.out.println("subList after= " + subList);
+        System.out.println("integers after= " + integers);
     }
+
 
     // класс Кошка
     static class Cat implements Sound {
@@ -55,5 +76,19 @@ public class JavaProMain {
             System.out.println("meow"); // мяу
 
         }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Cat cat = (Cat) o;
+            return Objects.equals(color, cat.color) && Objects.equals(name, cat.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(color, name);
+        }
     }
+
 }
