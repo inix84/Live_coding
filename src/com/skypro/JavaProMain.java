afaab961fa5966c10b0fa6d45f464b267ba5f78e
inix84 <inix84@mail.ru>
2025-02-23 15:35:29 +0500

    карты(мапы) по лайву 7

--- src/com/skypro/JavaProMain.java
+++ src/com/skypro/JavaProMain.java
@@ -1,9 +1,6 @@
 package com.skypro;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 public class JavaProMain {
     public static void main(String[] args) throws InterruptedException {
@@ -36,10 +33,38 @@ public class JavaProMain {
         // если какой то ключ меняется, то значение потом по этому ключу мы достатть не сможем
         System.out.println("myMap.containsKey((peremennay)) = " + myMap.containsKey((peremennay)));
         System.out.println("myMap.size() = " + myMap.size()); // размер тот же, но значение потерялось
-    
-    
+        peremennay.setKey(1);
+        System.out.println("myMap.containsKey((peremennay)) = " + myMap.containsKey((peremennay)));
+// если какокго значения в карте нет, то создать его оперативно и работать с ним
+        MyValue myValue = myMap.get(new MyKey(4)); // получить значение моей карты по ключику 4
+        if (myValue == null) {
+            myValue = new MyValue("white", 42);
+            System.out.println("myValue = " + myValue);
+        }
+        // верхние 4 строчки можно переписать одной строчкой
+
+        System.out.println("myValue Or Default compute If Absent= " + myMap.computeIfAbsent(new MyKey(4), k -> new MyValue("white", 99))); // если ключа нет. то создаем, еслиесть, то возвращает старое значение
+        System.out.println("myValue Or Default = " + myMap.getOrDefault(new MyKey(4), new MyValue("white", 42)));
+        myMap.putIfAbsent(new MyKey(4), new MyValue("brown", 1)); // принимает в себя заранее созданые значения, а не значения, кот вычисляются позже
+        System.out.println("myMap put If Absent= " + myMap);
+// есть мапы не содержащие всех методов, например ТРИМАП
+
+        NavigableMap<Integer, String> cities = new TreeMap<>(); //первый интерфейс мапы, который поддерживает порядок NavigableMap, такой легче куда то передать
+        cities.put(1, "Воркута");
+        cities.put(100, "Москва");
+        cities.put(10, "С-Пб");
+        System.out.println("cities = " + cities); // распечатается в порядке возрастания ключей
+        System.out.println("cities.get(100) = " + cities.get(100));
+        System.out.println("cities.ceilingEntry(50) = " + cities.ceilingEntry(100)); // метод до ПОТОЛКА 50
+        System.out.println("cities.ceilingEntry(50) = " + cities.higherEntry(100)); // строго выше потолка
+
+        Map<Integer, String> unmodifiableMap = Map.of(1,"Воркута",100, "Москва",10, "С-Пб");
+        System.out.println(" НЕИЗМЕНЯЕМО unmodifiableMap = " + unmodifiableMap);
+        //unmodifiableMap.put(4,"Челябинск");
+
+        Map<Integer, String> unmodifiableMap2 = Collections.unmodifiableMap(cities); // уже существующую мапу
+        System.out.println(" НЕИЗМЕНЯЕМО unmodifiableMap2 = " + unmodifiableMap2);
     }
-    
 
     static class MyKey {
         private int key;
@@ -84,7 +109,7 @@ public class JavaProMain {
             }
             //если ссылки не сработают, давайте наш обьект обжекст приводить к класу майкей.
             if (obj instanceof MyKey otherKey) { // оператор сравнения instanceof, перемееная otherKey
-// сравнивать мы будем по полям (создадим гет у майкей.)
+// сравнивать мы будем по полям (создадим гет у май-кей.)
                 return this.key == otherKey.getKey();
             } else {
                 return false;
